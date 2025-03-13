f64af446e331dc7c933bf596afbd166ccc7ab18a
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-03-13 16:25:32 +0100

    Lab 1

--- Lab 1 - rozgrzewka/main.js
+++ Lab 1 - rozgrzewka/main.js
@@ -1,9 +1,10 @@
-// notatnik z zajęć
-const liczba1 = document.querySelector('#liczba1')
-const btnPrzelicz = document.querySelector('#przelicz')
-const wynikiPojemnik = document.querySelector('#wyniki')
-
-btnPrzelicz.addEventListener('click', () => {
-    wynikiPojemnik.innerHTML = liczba1.value
-    console.log(liczba1.value)
-})
+// // notatnik z zajęć
+// const liczba1 = document.querySelector('#liczba1')
+// const btnPrzelicz = document.querySelector('#przelicz')
+// const wynikiPojemnik = document.querySelector('#wyniki')
+//
+// btnPrzelicz.addEventListener('click', () => {
+//     wmain.js:7 Uncaught TypeError: Cannot read properties of null (reading 'value')
+//     at HTMLButtonElement.<anonymous>ynikiPojemnik.innerHTML = liczba1.value
+//     console.log(liczba1.value)
+// })
